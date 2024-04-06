883b848fb9cef97f8e5ce433693a9e7e61ccb7ee
Mohamed <mohammedmohamuud.01@gmail.com>
2024-04-06 18:41:47 +0300

    Completed the intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fultter_e_commerce/components/button.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,6 +8,51 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Logo
+            Icon(
+              Icons.shopping_bag_outlined,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+            // Title
+            const Text(
+              "Minimal Shop",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+            // Subtitle
+            Text(
+              "Premium Quality Products",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+            // Button
+            Button(
+              onTap: () {},
+              child: const Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
